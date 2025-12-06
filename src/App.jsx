b72b8f3ce508b38eebb257db5d45b11290a5e314
PayshviGhodwall <payshvighodwallmr_cse17@its.edu.in>
2025-12-07 00:01:59 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const queryClient = new QueryClient({
 const LoadingFallback = () => (
   <div className="d-flex justify-content-center align-items-center vh-100">
     <div className="spinner-border text-primary" role="status">
-      <span className="visually-hidden">Loading...</span>
+      <span className="visually-hidden">Loading..</span>
     </div>
   </div>
 );
